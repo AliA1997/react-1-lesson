9c401052f5ba39aa4cf94417c1c17107472445c1
Tyler Collier <tyler.collier+github@devmounta.in>
2018-01-26 12:19:35 -0700

    Website with button and can add name

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newName: '',
+      names: ['Tyler', 'Adam', 'Sally']
+    };
+    this.addName = this.addName.bind(this);
+    this.updateNewName = this.updateNewName.bind(this);
+  }
+
+  addName() {
+    this.setState({
+      names: [...this.state.names, this.state.newName]
+    });
+  }
+
+  updateNewName(e) {
+    this.setState({
+      newName: e.target.value
+    });
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div>Hello this is my test because I'm not creative :-(</div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div className="my-list">
+          <ul>
+            {this.state.names.map(e => <li>{e}</li>)}
+          </ul>
+          Type your new name: <input onChange={this.updateNewName} value={this.state.newName} />
+          <button onClick={this.addName}>Add name</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
